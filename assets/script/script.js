fb318dd67c7c447de630e2cad3447609e2f74517
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-03-31 16:10:24 +1100

    created selected pokemon array with two mons; created for-each function to grab mon data

--- assets/script/script.js
+++ assets/script/script.js
@@ -18,9 +18,62 @@ function getPokeApi() {
         })
         .then(function (allPokemonData) {
             // console.log returned results to view all creatures
+            console.log("1. all pokemon data:");
             console.log(allPokemonData);
+
+            let pokemon = allPokemonData.results;
+
+            let selectedPokemon = [
+                pokemon[401], // bug type: kricketune
+                pokemon[570] // dark type: zoroark
+            ];
+
+            console.log("2. selected pokemon array:");
+            console.log(selectedPokemon);
+
+            selectedPokemon.forEach(function(pokemon) {
+                // pass info on
+                getPokeData(pokemon);
+            })  
+
+            
         })
         .catch(function (error) {
             console.log(error);
         });
 }
+
+var types = {
+    "bug": [],
+    "dark": [],
+    "dragon": [],
+    "electric": [],
+    "fairy": [],
+    "fighting": [],
+    "fire": [],
+    "flying": [],
+    "ghost": [],
+    "grass": [],
+    "ground": [],
+    "ice": [],
+    "normal": [],
+    "poison": [],
+    "psychic": [],
+    "rock": [],
+    "steel": [],
+    "water": [],
+    "random": []
+}
+
+function getPokeData(pokemon) {
+    console.log("3. get poke data function:");
+    console.log(pokemon);
+
+    let url = pokemon.url;
+
+    console.log("4. pokemon url");
+    console.log(url);
+}
+
+
+getPokeApi();
