594b75753fe98ab952eeba924d0e95170883cbd2
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-03-31 17:08:02 +1100

    separated grabbing mon name to its own function; added some comments

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,37 @@
 // POKEMON API functions
 
+// these variables may not end up being needed
+// var typesObj = {
+//     "bug": [],
+//     "dark": [],
+//     "dragon": [],
+//     "electric": [],
+//     "fairy": [],
+//     "fighting": [],
+//     "fire": [],
+//     "flying": [],
+//     "ghost": [],
+//     "grass": [],
+//     "ground": [],
+//     "ice": [],
+//     "normal": [],
+//     "poison": [],
+//     "psychic": [],
+//     "rock": [],
+//     "steel": [],
+//     "water": [],
+//     "random": []
+// }
+
+// variable to store pokemon data separately
+var pokeName = [];
+var pokeType = [];
+var pokeEntry = [];
+
+// set up empty pokemon object to push name, type and data thru later
+var pokemonObj = {};
+
+
 // connect to Pokemon API
 
 // first call to pokéAPI: fetch data for all pokémon
@@ -23,56 +55,61 @@ function getPokeApi() {
 
             let pokemon = allPokemonData.results;
 
+            // initial array to hold specific pokemon
             let selectedPokemon = [
                 pokemon[401], // bug type: kricketune
                 pokemon[570] // dark type: zoroark
             ];
-
+            // view the selected pokemon array
             console.log("2. selected pokemon array:");
             console.log(selectedPokemon);
 
+            // loop thru each pokemon in array to get required info
             selectedPokemon.forEach(function(pokemon) {
-                // pass info on
-                getPokeData(pokemon);
-            })  
+                // pass on info to get the pokemon name
+                getPokeName(pokemon.name);
+                // pass on url data to next function
+                getPokeData(pokemon.url);
+            });
 
-            
         })
         .catch(function (error) {
             console.log(error);
         });
 }
 
-var types = {
-    "bug": [],
-    "dark": [],
-    "dragon": [],
-    "electric": [],
-    "fairy": [],
-    "fighting": [],
-    "fire": [],
-    "flying": [],
-    "ghost": [],
-    "grass": [],
-    "ground": [],
-    "ice": [],
-    "normal": [],
-    "poison": [],
-    "psychic": [],
-    "rock": [],
-    "steel": [],
-    "water": [],
-    "random": []
+// get the name of the pokemon
+function getPokeName(name) {
+    // grab the pokemon name and push it to pokeName array
+    pokeName.push(name);
+    // view the name array
+    console.log("POKE NAME ARRAY:");
+    console.log(pokeName);
 }
 
-function getPokeData(pokemon) {
+// get the url of the pokemon (for further data extrapolation)
+function getPokeData(pokemonUrl) {
     console.log("3. get poke data function:");
-    console.log(pokemon);
-
-    let url = pokemon.url;
+    console.log(pokemonUrl);
 
     console.log("4. pokemon url");
-    console.log(url);
+    console.log(pokemonUrl);
+
+    fetch(pokemonUrl)
+        .then(function (response) {
+            if (!response.ok) {
+                throw response.json();
+            } else {
+                return response.json();
+            }
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+
 }
 
 
