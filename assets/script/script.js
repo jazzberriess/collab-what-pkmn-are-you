2cc97564387280895528ff4d6fa91fa693c0898b
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-04-01 13:04:30 +1100

    created pkmn img array; fetched artwork url and species data url

--- assets/script/script.js
+++ assets/script/script.js
@@ -92,15 +92,16 @@ artistBtn.addEventListener("click", getToken)
 //     "random": []
 // }
 
-// variable to store pokemon data separately
-var pokeName = [];
-var pokeType = [];
-var pokeEntry = [];
+// arrays to store pokemon data separately
+var pokeName = [];  // for the pokemon name
+var pokeType = [];  // for the pokemon typing
+var pokeArtwork = []; // for the pokemon's official artwork url
+var pokeEntry = []; // for the pokemon's species description
 
 // set up empty pokemon object to push name, type and data thru later
 var pokemonObj = {};
 
-// connect to Pokemon API
+// CONNECT TO pokéAPI
 
 // first call to pokéAPI: fetch data for all pokémon
 function getPokeApi() {
@@ -122,7 +123,24 @@ function getPokeApi() {
             // initial array to hold specific pokemon
             let selectedPokemon = [
                 pokemon[401], // bug type: kricketune
-                pokemon[570] // dark type: zoroark
+                pokemon[570], // dark type: zoroark
+                pokemon[380], // dragon type: latios
+                pokemon[178], // electric type: mareep
+                pokemon[699], // fairy type: sylveon
+                pokemon[891], // fighting type: urshifu
+                pokemon[725], // fire type: torracat
+                pokemon[16], // flying type: pidgeotto
+                pokemon[353], // ghost type: banette
+                pokemon[406], // grass type: roserade
+                pokemon[220], // ground type: piloswine
+                pokemon[711], // ice type: bergmite
+                pokemon[107], // normal type: lickitung
+                pokemon[434], // poison type: skuntank
+                pokemon[857], // psychic type: hatterene
+                pokemon[525], // rock type: gigalith
+                pokemon[305], // steel type: aggron
+                pokemon[729], // water type: primarina
+                pokemon[772], // random choice: silvally
             ];
 
             // loop thru each pokemon in array to get required info
@@ -131,6 +149,8 @@ function getPokeApi() {
                 getPokeName(pokemon.name);
                 // pass on url data to function for grabbing type
                 getPokeType(pokemon.url);
+                // pass on url to function for grabbing species data
+                getPokeSpecies(pokemon.url);
             });
         })
         .catch(function (error) {
@@ -142,9 +162,6 @@ function getPokeApi() {
 function getPokeName(name) {
     // grab the pokemon name and push it to pokeName array
     pokeName.push(name);
-    // view the name array
-    console.log("POKE NAME ARRAY:");
-    console.log(pokeName);
 }
 
 // get the pokemon's type through second fetch call
@@ -159,24 +176,53 @@ function getPokeType(pokemonUrl) {
             }
         })
         .then(function(pokeData) {
-            // first console.log to make sure our data has been correctly pulled
-            console.log("url data: ");
-            console.log(pokeData);
-
             // for readability, make a variable 'type' and assign it the fetched data
             var type = pokeData.types[0].type.name;
-            // console log for good measure
-            console.log("type");
-            console.log(type);
 
             // push the type to the pokeType array
             pokeType.push(type);
-            console.log("type as array");
-            console.log(pokeType);
         })
         .catch(function (error) {
             console.log(error);
+        });
+}
+
+function getPokeSpecies(pokemonUrl) {
+    // fetch data using pokemon.url
+    fetch(pokemonUrl)
+        .then(function (response) {
+            if (!response.ok) {
+                throw response.json();
+            } else {
+                return response.json();
+            }
         })
+        .then(function (pokeData) {
+            // grab url for official artwork
+            var officialArt = pokeData.sprites.other["official-artwork"].front_default;
+            // turn it into a string using template literals (template strings)
+            var artUrl = `"${officialArt}"`;
+            // pass art url on
+            getPokeArt(artUrl);
+
+            // grab species data url
+            var species = pokeData.species.url;
+            // pass it on to get flavour text
+            getPokeFlavourText(species);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+// to get the official artwork img url
+function getPokeArt(arturl) {
+    console.log(arturl);
+}
+
+// to get the pokemon's pokedex entry (species flavour text)
+function getPokeFlavourText(species) {
+    console.log(species);
 }
 
 
