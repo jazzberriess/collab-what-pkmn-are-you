27ef09dc846ab87fc9d203769f1398f1888f0be4
Jazzberries <anime.flick.chick@gmail.com>
2022-04-04 21:29:22 +1000

    commiting to merge main into branch

--- assets/script/script.js
+++ assets/script/script.js
@@ -747,27 +747,32 @@ function appendElements() {
 
     //display Pokémon name
     // let yourPkmnDisplay = document.createElement("div");
-    resultsDisplay.innerHTML = "You are " + yourPkmn.name + "!";
+    // resultsDisplay.innerHTML = "You are " + yourPkmn.name + "!";
     // resultsDisplay.appendChild(yourPkmnDisplay);
-
-    //display Pokémon type
-    let yourPkmnType = document.createElement("div");
-    yourPkmnType.innerHTML = "Type: " + yourPkmn.type;
-    // yourPkmnDisplay.appendChild(yourPkmnType);
-    resultsDisplay.appendChild(yourPkmnType);
+    let pokemonName = document.getElementById("pokemon-name");
+    pokemonName.textContent = yourPkmn.name + "!";
+    resultsDisplay.append(pokemonName);
 
     //display Pokémon image
-    let yourPkmnImage = document.createElement("img");
+    let yourPkmnImage = document.getElementById("pkmn-image");
     yourPkmnImage.setAttribute("src", yourPkmn.artwork);
     yourPkmnImage.setAttribute("alt", "Official artwork of the Pokémon.");
     // yourPkmnDisplay.appendChild(yourPkmnImage);
-    resultsDisplay.appendChild(yourPkmnImage);
+    resultsDisplay.append(yourPkmnImage);
 
     //display Pokémon info
-    let yourPkmnInfo = document.createElement('div');
-    yourPkmnInfo.innerHTML = "Info: " + yourPkmn.entry;
+    let yourPkmnInfo = document.getElementById("pkmn-info");
+    yourPkmnInfo.textContent = "Type: " + yourPkmn.type + ". Info: " + yourPkmn.entry;
     // yourPkmnDisplay.appendChild(yourPkmnInfo);
-    resultsDisplay.appendChild(yourPkmnInfo);
+    resultsDisplay.append(yourPkmnInfo);
+
+    //display Pokémon type
+    //  let yourPkmnType = document.createElement("div");
+    //  yourPkmnType.innerHTML = "Type: " + yourPkmn.type;
+    // yourPkmnDisplay.appendChild(yourPkmnType);
+    //  resultsDisplay.appendChild(yourPkmnType);
+
+
 
     //display Pokémon ability
     let yourPkmnAbility = document.createElement("div");
@@ -844,12 +849,16 @@ function retrieveResults() {
         // run through each item in local storage
         for (let i = 0; i < localData.length; i++) {
             // make a list item and add text referencing artist name, pokemon type, and pokemon name
+            historyDisplay.removeAttribute("class");
+            hideHistoryBtn.removeAttribute("class");
+            clearHistoryBtn.removeAttribute("class");
+            historyBtn.setAttribute("class", "hidden");
             let line = document.createElement("li");
             line.textContent = `Choosing ${localData[i].artist} means you are the ${localData[i].type} type Pokémon, ${localData[i].pokemon}!`;
 
             // append the elements to each other
             resultsList.appendChild(line)
-            resultsDisplay.appendChild(resultsList);
+            historyDisplay.appendChild(resultsList);
         }
     }
 }
@@ -885,3 +894,9 @@ init();
 
 //THIS EVENT LISTENER WILL NEED TO CHANGE TO THE FORM SUBMIT BUTTON WHEN WE CREATE THE USER INPUT FIELD
 artistBtn.addEventListener("click", matchArtistToPokemon)
+
+historyBtn.addEventListener("click", retrieveResults);
+
+hideHistoryBtn.addEventListener("click", hideHistory);
+
+clearHistoryBtn.addEventListener("click", clearHistory);
